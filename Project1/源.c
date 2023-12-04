8517943ac1a7ceb27bef04567e055d25bdb55e06
FrankAaron <2741218588@qq.com>
2023-12-04 21:03:07 +0800

    实验2

--- Project1/源.c
+++ Project1/源.c
@@ -1,22 +1,23 @@
+//实验	有一个已经排好序的数组，要求输入一个数后，按原来排序的规律将它插入数组中
+
 // 软件2316 朱昱杰
 #include"stdio.h"
+
 int main() {
-	for (int i = 2; i < 1000; i++) {
-		int sum = 0;
-		for (int j = 1; j < i; j++) {
-			if (i % j == 0) {
-				sum += j;
+	int arr[8] = { 1,2,3,4,5,7,8 };
+	int m;
+	scanf_s("%d",&m);
+	for (int i = 0; i < 8; i++) {
+		if (arr[i] >= m) {
+			for (int j = 7; j > i; j--) {
+				arr[j] = arr[j - 1];
 			}
+			arr[i] = m;
+			break;
 		}
-		if (sum == i) {
-			printf("%d its factors are ", i);
-			for (int j = 1; j < i; j++) {
-				if (i % j == 0) {
-					printf("%d,", j);
-				}
-			}
-			printf("\n");
-		}
+	}
+	for (int i = 0; i < 8; i++) {
+		printf("%d",arr[i]);
 	}
 	return 0;
 }
