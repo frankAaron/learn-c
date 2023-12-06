34f23f83cd20e59bac8f1a353f9540c10e97d29f
FrankAaron <2741218588@qq.com>
2023-12-06 16:24:35 +0800

    x的n次方

--- Project1/源.c
+++ Project1/源.c
@@ -1,27 +1,20 @@
-//实验  	设计一函数，判断一个字符串是否是回文。“回文”是一种顺序读和反序读都一样的字符串，
-// 例如：“121” “abcba” “ABCCBA”。在主函数中输入字符串并输出判断结果。
 //软件2316 朱昱杰
-#include <stdio.h>
-#include <string.h>
-
-int palindrome(char str[100]) {
-    int l = strlen(str);
-    for (int i = 0; i < l / 2; i++) {
-        if (str[i] != str[l - i - 2]) {
-            return 0;
-        }
-    }
-    return 1;
+#include<stdio.h>
+#include <math.h>
+int fection(int x,int n,int y) {
+	if (n > 0) {
+		y *= x;
+		return fection(x, n - 1, y);
+	}
+	else {
+		return y;
+	}
 }
-
 int main() {
-    char str[100];
-    fgets(str, sizeof(str), stdin); 
-    if (palindrome(str) == 1) {
-        printf("is");
-    }
-    else {
-        printf("is not");
-    }
-    return 0;
+	int x, n, y = 1;
+	printf("底数：");
+	scanf_s("%d",&x);
+	printf("指数：");
+	scanf_s("%d",&n);
+	printf("answer is :%d",fection(x, n, y));
 }
